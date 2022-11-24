f3cdd3b74114b37f0ba2e7750d88f5c76d20aa40
PPekKunGz <kittipat25er@gmail.com>
2022-11-25 06:57:05 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var convertBtn = document.querySelector(".convert_btn");
+var URLinput = document.querySelector(".url_input");
+
+convertBtn.addEventListener("click", function () {
+  console.log(`URL : ${URLinput.value}`);
+  sendURL(URLinput.value);
+});
+
+function sendURL(URL) {
+  window.location.href = `http://45.130.141.14:4000/download?URL=${URL}`;
+}
