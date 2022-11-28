1ddecf8aeb28ab030b4ea935e44295c32b56dc01
PPekKunGz <kittipat25er@gmail.com>
2022-11-29 03:42:06 +0700

    update

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ convertBtn.addEventListener("click", function () {
 });
 
 function sendURL(URL) {
-  window.location.href = `http://45.130.141.14:4000/download?URL=${URL}`;
+  window.location.href = `http://localhost:4000/download?URL=${URL}`;
 }
